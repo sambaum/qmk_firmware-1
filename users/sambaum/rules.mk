82ca1595b12ccef172607cf7fd3dcbafd87adf36
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2021-10-07 22:26:31 +0200

    rules.mk reorder

--- users/sambaum/rules.mk
+++ users/sambaum/rules.mk
@@ -1,8 +1,8 @@
-RGB_MATRIX_CUSTOM_USER = yes
 SRC += sambaum.c
+
+RGB_MATRIX_CUSTOM_USER = yes
+KEY_OVERRIDE_ENABLE = yes
 
 ifeq ($(strip $(MCU)), atmega32u4)
   LTO_ENABLE = yes
 endif
-
-KEY_OVERRIDE_ENABLE = yes
