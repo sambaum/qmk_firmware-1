8e3900d28108ab0fb63f5c65e2a5d8391c135489
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2022-06-12 15:31:18 +0200

    add chaos keyboard

---
+++ keyboards/chaos/config.h
@@ -0,0 +1,52 @@
+/* Copyright 2021 Your Name (@sambaum)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x1688
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    THOMASLT STUDIO
+#define PRODUCT         Chaos
+
+/* key matrix size */
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 15
+
+/*
+ * Keyboard Matrix Assignments
+ *
+ * Change this to how you wired your keyboard
+ * COLS: AVR pins used for columns, left to right
+ * ROWS: AVR pins used for rows, top to bottom
+ * DIODE_DIRECTION: COL2ROW = COL = Anode (+), ROW = Cathode (-, marked on diode)
+ *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
+ *
+*/
+#define MATRIX_ROW_PINS { D0, D1, D2, D3, D5 }
+#define MATRIX_COL_PINS { B0, B1, B2, B3, D4, D6, D7, B4, B5, B6, C6, F7, F6, F5, F4 }
+#define FORCE_NKRO
+#define DIODE_DIRECTION COL2ROW
+
+//#define RGBLED_NUM      18
+//#define RGB_DI_PIN      C7  //reserved pin for potential underglow rgb 
+//#define RGBLIGHT_ANIMATIONS
+//#define RGBLIGHT_HUE_STEP 8
+//#define RGBLIGHT_SAT_STEP 8
+//#define RGBLIGHT_VAL_STEP 8
---
+++ keyboards/chaos/readme.md
@@ -0,0 +1,17 @@
+# Chaos
+
+![Chaos](https://static.wixstatic.com/media/c336a9_1a421601fd504e0b9474024b933e0e4c~mv2.jpg)
+
+65% Keyboard. More info on [www.thomasltstudio.com](https://www.thomasltstudio.com/)
+
+* Keyboard Maintainer: Community
+* Hardware Supported: Chaos PCB Atmega32u4
+* Hardware Availability: [www.thomasltstudio.com](https://www.thomasltstudio.com/)
+
+Reset using the reset button.
+
+Make example for this keyboard (after setting up your build environment):
+
+    make chaos:default
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).09
