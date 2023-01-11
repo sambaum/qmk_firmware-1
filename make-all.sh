885ecef3dffda796a9a18e9af2cd0f302f3a13c9
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2023-01-11 13:45:59 +0100

    add some convenience

--- make-all.sh
+++ make-all.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+
+echo
+while [ -n "$1" ]; do
+	case "$1" in
+	-f)
+		echo "Deleting $SCRIPT_DIR/.build folder..."
+		rm -rf $SCRIPT_DIR/.build
+		;;
+	*) echo "$1 is not an option" ;;
+	esac
+	shift
+done
+
 make dztech/dz65rgb/v2:sambaum
 make kprepublic/bm16a:sambaum
 make kprepublic/bm68hsrgb/rev1:sambaum
